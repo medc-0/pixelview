970438932af5f348490abc02a3c32caf154d2b67
medc-0 <guestmoon5@gmail.com>
2026-01-14 23:24:56 +0100

    feat: implement initial window pixel viewer with SDL2

---
+++ pixelViewCore.cpp
@@ -0,0 +1,38 @@
+#define SDL_MAIN_HANDLED
+#include <SDL2/SDL.h>
+#include <iostream>
+
+#define WIDTH 1080
+#define HEIGHT 720
+
+int main() {
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        std::cerr << "SDL_Init Error: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    SDL_Window *window = SDL_CreateWindow("PixelView v0.1.0", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN);
+    if (window == nullptr) {
+        std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Surface *surface = SDL_GetWindowSurface(window);
+
+    bool running = true;
+    SDL_Event event;
+
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT)
+                running = false;
+        }
+        SDL_FillRect(surface, nullptr, SDL_MapRGB(surface->format, 0, 0, 0));
+        SDL_UpdateWindowSurface(window);
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
+}
