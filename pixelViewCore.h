7f675e6050cdc97bb18c1d710d717855f296c524
medc-0 <guestmoon5@gmail.com>
2026-01-15 16:20:23 +0100

    refactor: header file and adding more compatibility

--- pixelViewCore.h
+++ pixelViewCore.h
@@ -3,14 +3,13 @@
 
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
-#include <iostream>
 #include <string>
 
 class ImageViewer {
 public:
     ImageViewer(const std::string& filename);
     ~ImageViewer();
-    void display();
+    void run();
 
 private:
     SDL_Window* window;
@@ -18,7 +17,21 @@ private:
     SDL_Texture* texture;
     int width;
     int height;
+
+    int imgW;
+    int imgH;
+
+    float zoom;
+    float offsetX;
+    float offsetY;
+
+    bool dragging;
+    int lastX;
+    int lastY;
+
     void loadImage(const std::string& filename);
+    void fitToWindow();
+    void renderer();
 };
 
 #endif
